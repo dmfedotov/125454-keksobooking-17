bdf6f40755322ad21c8be28aa366b91fba3304d7
Дмитрий <crismsfiva@gmail.com>
2019-09-17 17:12:19 +0300

    Добавляет странице неактивное состояние

--- js/change-page-state.js
+++ js/change-page-state.js
@@ -1,12 +1,20 @@
 'use strict';
 
 (function () {
-  window.changePageState = function () {
-    window.pins.map.classList.remove('map--faded');
-    window.address.adsForm.classList.remove('ad-form--disabled');
+  window.changePageState = function (status) {
+    if (status) {
+      window.pins.map.classList.remove('map--faded');
+      window.address.adsForm.classList.remove('ad-form--disabled');
 
-    window.changeFormState(window.address.adsForm, true);
-    window.changeFormState(window.filters.form, true);
-    window.backend.donwload(window.preview.onLoad, window.error.show);
+      window.changeFormState(window.address.adsForm, true);
+      window.changeFormState(window.filters.form, true);
+      window.backend.donwload(window.preview.onLoad, window.error.show);
+    } else {
+      window.pins.map.classList.add('map--faded');
+      window.address.adsForm.classList.add('ad-form--disabled');
+
+      window.changeFormState(window.address.adsForm, false);
+      window.changeFormState(window.filters.form, false);
+    }
   };
 })();
