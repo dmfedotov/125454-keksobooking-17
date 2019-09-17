ae1183b395ee06d892da2f9a783e57f9d535192d
Дмитрий <crismsfiva@gmail.com>
2019-09-17 17:11:15 +0300

    Устанавливает адрес метки пользователя по умолчанию

--- js/address.js
+++ js/address.js
@@ -2,9 +2,9 @@
 
 (function () {
   var form = document.querySelector('.ad-form');
+  var addressField = form.querySelector('#address');
 
   var fillAddress = function (pageState) {
-    var addressField = form.querySelector('#address');
     var coordX = parseInt(window.pins.userPin.style.left, 10);
     var coordY = parseInt(window.pins.userPin.style.top, 10);
 
@@ -21,8 +21,15 @@
     addressField.value = coordX + ', ' + coordY;
   };
 
+  var setDefaultAddress = function () {
+    var addressX = parseInt(window.pins.userPin.style.left, 10);
+    var addressY = parseInt(window.pins.userPin.style.top, 10);
+    addressField.value = addressX + ', ' + addressY;
+  };
+
   window.address = {
     adsForm: form,
-    fill: fillAddress
+    fill: fillAddress,
+    setDefault: setDefaultAddress
   };
 })();
