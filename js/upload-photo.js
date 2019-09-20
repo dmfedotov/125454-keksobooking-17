bb5fed7266112b53a19da883bd8a74aeabe4604e
Дмитрий <crismsfiva@gmail.com>
2019-09-20 13:05:09 +0300

    Позволяет загрузить фотографии жилья

---
+++ js/upload-photo.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'png', 'jpeg', 'jpg'];
+  var PhotoSize = {
+    WIDTH: 70,
+    HEIGHT: 70
+  };
+  var photoContainer = document.querySelector('.ad-form__photo-container');
+  var photoChooser = photoContainer.querySelector('#images');
+  var photoWrap = photoContainer.querySelector('.ad-form__photo');
+
+  var deleteEmptyPhotos = function () {
+    if (!photoContainer.querySelector('.ad-form__photo img')) {
+      photoContainer.querySelectorAll('.ad-form__photo').forEach(function (preview) {
+        photoContainer.removeChild(preview);
+      });
+    }
+  };
+
+  var createPhoto = function (src) {
+    var wrapper = photoWrap.cloneNode();
+    var image = new Image(PhotoSize.WIDTH, PhotoSize.HEIGHT);
+    image.src = src;
+    image.alt = 'Фотография жилья';
+    image.style.borderRadius = '5px';
+
+    wrapper.appendChild(image);
+
+    return wrapper;
+  };
+
+  var showUploadedPhotos = function (files) {
+    deleteEmptyPhotos();
+    files.forEach(function (file) {
+      var fileName = file.name.toLowerCase();
+      var mathes = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
+      });
+
+      if (mathes) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          var photo = createPhoto(reader.result);
+          photoContainer.appendChild(photo);
+        });
+
+        reader.readAsDataURL(file);
+      }
+    });
+  };
+
+  photoChooser.addEventListener('change', function () {
+    var files = Array.from(photoChooser.files);
+    showUploadedPhotos(files);
+  });
+})();
