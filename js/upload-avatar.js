1f2aaf2bcea63997c2f413203dd4e22d20e7f879
Дмитрий <crismsfiva@gmail.com>
2019-09-22 13:12:22 +0300

    Позволяет удалить загруженный аватар

--- js/upload-avatar.js
+++ js/upload-avatar.js
@@ -2,8 +2,31 @@
 
 (function () {
   var FILE_TYPES = ['gif', 'png', 'jpeg', 'jpg'];
+  var DEFAULT_AVATAR_SRC = 'img/muffin-grey.svg';
   var avatarChooser = document.querySelector('#avatar');
-  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+  var avatarWrap = document.querySelector('.ad-form-header__preview');
+  var avatarPreview = avatarWrap.querySelector('img');
+
+  var deleteUploadedAvatar = function () {
+    avatarPreview.src = DEFAULT_AVATAR_SRC;
+    avatarChooser.value = '';
+    avatarWrap.removeEventListener('mouseenter', onMouseEnter);
+    avatarWrap.removeEventListener('mouseover', onMouseLeave);
+  };
+
+  var onMouseEnter = function () {
+    avatarWrap.classList.add('ad-form-header__preview--delete');
+    avatarWrap.addEventListener('click', onAvatarClick);
+  };
+
+  var onMouseLeave = function () {
+    avatarWrap.classList.remove('ad-form-header__preview--delete');
+    avatarWrap.removeEventListener('click', onAvatarClick);
+  };
+
+  var onAvatarClick = function () {
+    deleteUploadedAvatar();
+  };
 
   avatarChooser.addEventListener('change', function name() {
     var file = avatarChooser.files[0];
@@ -18,10 +41,16 @@
 
       reader.addEventListener('load', function () {
         avatarPreview.src = reader.result;
-      });
 
+        avatarWrap.addEventListener('mouseenter', onMouseEnter);
+        avatarWrap.addEventListener('mouseleave', onMouseLeave);
+      });
 
       reader.readAsDataURL(file);
     }
   });
+
+  window.uploadAvatar = {
+    delete: deleteUploadedAvatar
+  };
 })();
